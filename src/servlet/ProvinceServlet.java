f6a29ea887e35aea99246d31710320921442f648
wjchen <wjchen1015@163.com>
2020-03-12 08:29:07 +0800

    完善地图请求处理

--- src/servlet/ProvinceServlet.java
+++ src/servlet/ProvinceServlet.java
@@ -25,6 +25,7 @@ import com.sun.java_cup.internal.runtime.virtual_parse_stack;
 import dao.ProvinceDAO;
 import dao.ProvinceDAOImpl;
 import pojo.Province;
+import util.DateUtil;
 
 /**
  * Servlet implementation class test
@@ -54,40 +55,79 @@ public class ProvinceServlet extends HttpServlet {
 		String year = request.getParameter("year");
 		String month = request.getParameter("month");
 		String day = request.getParameter("day");
+		String type = request.getParameter("type");
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		Map<String, Object> map = new HashMap<String, Object>();
 		List<String> provinceName = new ArrayList<String>();
 		List<Integer> provinceValue = new ArrayList<Integer>();
 		List<Integer> nation = new ArrayList<Integer>();
-		if (year != null && month != null && day != null) {
-			System.out.println(year + "-" + month + "-" + day);
-			String dateString = year + "-" + month + "-" + day;
-			try {
-				Province data[] = provinceDAO.getList(new Date(format.parse(dateString).getTime()));
-				for (Province p : data) {
-					if (!p.getName().equals("全国")) {
-						provinceName.add(p.getName());
-						provinceValue.add(p.getNowIp());
-					} else {
-						nation.add(p.getNowIp());
-						nation.add(p.getNowSp());
-						nation.add(p.getAllIp());
-						nation.add(p.getAllSp());
-						nation.add(p.getAllCure());
-						nation.add(p.getAlldead());
-						map.put("全国", nation);
-					}
-				}
-				map.put("省名", provinceName);
-				map.put("值", provinceValue);
-				JSONObject json = JSONObject.parseObject(JSON.toJSONString(map));
-				out.write(json.toString());
-				out.flush();
-				out.close();
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		String dateString = "";
+		String yesterdayString="";
+		if (year != null && month != null && day != null && type != null) {
+			if (!year.equals("") && !month.equals("") && !day.equals("")) {
+				System.out.println(year + "-" + month + "-" + day);
+				dateString = year + "-" + month + "-" + day;
+			} else {
+				dateString = "2020-02-02";
 			}
+		}
+		else {
+			dateString = "2020-02-02";
+		}
+		try {
+			yesterdayString=DateUtil.getYesterday(dateString);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			if(format.parse(yesterdayString).getTime()<format.parse("2020-01-19").getTime()) {
+				nation.add(0);
+				nation.add(0);
+				nation.add(0);
+				nation.add(0);
+				nation.add(0);
+				nation.add(0);
+			}
+			else {
+				Date yesterday=new Date(format.parse(yesterdayString).getTime());
+				Province p=provinceDAO.get("全国", yesterday);
+				nation.add(p.getNowIp());
+				nation.add(p.getNowSp());
+				nation.add(p.getAllIp());
+				nation.add(p.getAllSp());
+				nation.add(p.getAllCure());
+				nation.add(p.getAlldead());
+			}
+			Province data[] = provinceDAO.getList(new Date(format.parse(dateString).getTime()));
+			for (Province p : data) {
+				if (!p.getName().equals("全国")) {
+					provinceName.add(p.getName());
+					if(type!=null&&type.equals("now")) {
+						provinceValue.add(p.getNowIp());
+					}
+					else {
+						provinceValue.add(p.getAllIp());
+					}
+				} else {
+					nation.add(p.getNowIp());
+					nation.add(p.getNowSp());
+					nation.add(p.getAllIp());
+					nation.add(p.getAllSp());
+					nation.add(p.getAllCure());
+					nation.add(p.getAlldead());
+					map.put("全国", nation);
+				}
+			}
+			map.put("省名", provinceName);
+			map.put("值", provinceValue);
+			JSONObject json = JSONObject.parseObject(JSON.toJSONString(map));
+			out.write(json.toString());
+			out.flush();
+			out.close();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
